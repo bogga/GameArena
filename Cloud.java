cdf9339900ca873854d500307d9059a92f2c363b
Harry <h.almond@lancaster.ac.uk>
2017-02-14 16:05:01 +0000

    Changed cloud pos

--- Cloud.java
+++ Cloud.java
@@ -4,8 +4,8 @@ public class Cloud
 	{
 		Ball array[] = new Ball[3];
 		array[0] = new Ball(x, y, 50, "WHITE");
-		array[1] = new Ball(x - 15, y - 5, 30, "WHITE");
-		array[2] = new Ball(x + 10, y + 5, 30, "WHITE");
+		array[1] = new Ball(x - 150, y - 5, 30, "WHITE");
+		array[2] = new Ball(x + 100, y + 5, 30, "WHITE");
 		for (int i = 0; i < 3; i++)
 			arena.addBall(array[i]);
 	}
