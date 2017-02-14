0fafc27fb2b42ab806f50619984e63edceaaa03f
Harry <h.almond@lancaster.ac.uk>
2017-02-14 16:18:52 +0000

    Added head to sheep

--- Sheep.java
+++ Sheep.java
@@ -1,7 +1,18 @@
-public class Sheep extends Rectangle{
+import java.util.Random;
+public class Sheep{
 
-public Sheep(int x, int y, int w, int h, java.lang.String col){
-	super (x, y, w, h, col);
+public static Random rand = new Random();
+
+public Sheep(int x, int y, int w, int h, java.lang.String col, GameArena arena){
+	if (rand.nextBoolean())
+		col = "WHITE";
+	else
+		col = "GRAY";
+	Ball head = new Ball(x+50, y-(h/2), 20, "PINK");
+	Rectangle body = new Rectangle(x, y, w, h, col);
+	arena.addRectangle(body);
+	arena.addBall(head);
+	
 }
 
 }
