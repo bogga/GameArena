dc5d20b105d14e38794898217f557e15ce03b755
Adam Bogg <adbogg+github@gmail.com>
2017-02-15 16:48:11 +0000

    Fence no longer extends rectangle class

--- Fence.java
+++ Fence.java
@@ -1,6 +1,7 @@
-public class Fence extends Rectangle {
-
-	public Fence(int x, int y, int w, int h, java.lang.String col, GameArena arena) {
+public class Fence
+{
+	public Fence(int x, int y, int w, int h, java.lang.String col, GameArena arena)
+	{
 		Rectangle gate = new Rectangle(x, y, w, h, col);
 		arena.addRectangle(gate);
 	}
