b47476432530f5f5a13929fa2fe217b47151edec
Harry Almond <h.almond@lancaster.ac.uk>
2017-02-18 20:59:54 +0000

    Sheep now look like they are facing forward

--- Sheep.java
+++ Sheep.java
@@ -10,16 +10,16 @@ public class Sheep
 		else
 			col = "GRAY";
 		Ball head = new Ball(x+50, y-(h/2), 20, "PINK");
-		Ball headCover = new Ball(x+50, y-(h/2)+7, 7, "PINK"); //headCover is to make it so facial features can be created
-		Ball mouth = new Ball(x+50, y-(h/2)+9, 7, "BLACK");    //i.e. a moth by using a headCover and a black Ball
-		Ball eyer = new Ball(x+42, y-(h/2), 3, "BLACK");
+		Ball headCover = new Ball(x+60, y-(h/2)+8, 7, "PINK"); //headCover is to make it so facial features can be created
+		Ball mouth = new Ball(x+60, y-(h/2)+10, 7, "BLACK");    //i.e. a moth by using a headCover and a black Ball
+		Ball eyer = new Ball(x+53, y-(h/2), 3, "BLACK");
 		Ball eyel = new Ball(x+58, y-(h/2), 3, "BLACK");
 		Ball arse = new Ball(x-40, y, 25, col);
 		Ball abdomen = new Ball(x, y, 40, col);
 		Ball neck = new Ball(x+40, y, 20, col);
 		Rectangle legBack = new Rectangle(x-30, y+30, 10, 50, "BLACK");
 		Rectangle legFront = new Rectangle(x+30, y+30, 10, 50, "BLACK");
-		
+
 		arena.addRectangle(legBack);
 		arena.addRectangle(legFront);
 		arena.addBall(arse);
@@ -27,7 +27,7 @@ public class Sheep
 		arena.addBall(neck);
 		arena.addBall(head);
 		arena.addBall(mouth);
-		arena.addBall(eyel);
+	//	arena.addBall(eyel);
 		arena.addBall(eyer);
 		arena.addBall(headCover);
 	}
