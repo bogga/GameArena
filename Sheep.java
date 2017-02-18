f8a4fa2393f9117a1e0006ca8212d785fab04414
Harry Almond <h.almond@lancaster.ac.uk>
2017-02-18 14:53:02 +0000

    Sheep looking pretty good, have legs, eyes, mouth, body in cloud style and can be added easily.

--- Sheep.java
+++ Sheep.java
@@ -10,19 +10,26 @@ public Sheep(int x, int y, int w, int h, java.lang.String col, GameArena arena){
 	else
 		col = "GRAY";
 	Ball head = new Ball(x+50, y-(h/2), 20, "PINK");
+	Ball headCover = new Ball(x+50, y-(h/2)+7, 7, "PINK");
+	Ball mouth = new Ball(x+50, y-(h/2)+9, 7, "BLACK");
 	Ball eyer = new Ball(x+42, y-(h/2), 3, "BLACK");
 	Ball eyel = new Ball(x+58, y-(h/2), 3, "BLACK");
 	Ball arse = new Ball(x-40, y, 25, col);
 	Ball abdomen = new Ball(x, y, 40, col);
 	Ball neck = new Ball(x+40, y, 20, col);
-	Rectangle body = new Rectangle(x, y, w, h, col);
-	//arena.addRectangle(body);
+	Rectangle legBack = new Rectangle(x-30, y+30, 10, 50, "BLACK");
+	Rectangle legFront = new Rectangle(x+30, y+30, 10, 50, "BLACK");
+	arena.addRectangle(legBack);
+	arena.addRectangle(legFront);
 	arena.addBall(arse);
 	arena.addBall(abdomen);
 	arena.addBall(neck);
 	arena.addBall(head);
+	arena.addBall(mouth);
 	arena.addBall(eyel);
 	arena.addBall(eyer);
+
+	arena.addBall(headCover);
 
 }
 
